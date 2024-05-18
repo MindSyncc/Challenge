d52b4425caf6dbd8529df600d9042e1e9a155bc0
Juan <juanfuentesrufino6@gmail.com>
2024-05-18 16:20:42 -0300

    Criando a função temporizador e o número de curvas

--- corrida.py
+++ corrida.py
@@ -1,5 +1,29 @@
 import random
-def pista():
-    tamanho_corrida = random.uniform(2.5, 3.0)
-    return tamanho_corrida
-print(f'{pista():.2f}')
+import time
+def tempo_corrida(minutos): #tempo corrida devera ser = 45
+    segundos = 60 * minutos
+    while segundos:
+        mins, secs = divmod(segundos, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end='\r')
+        time.sleep(1)
+        segundos -= 1
+    print('Acabou a corrida')
+
+
+tamanho_corrida = random.uniform(2.5, 3.0)
+attack_mode_posicao = random.uniform(0, tamanho_corrida)
+vel_carro = 0.1
+posicao = 0
+def curvas():
+    global tempo
+    num_curvas = random.randint(10, 20)
+    curvas_total = tempo * num_curvas
+def posicao_carro():
+    while True:
+        global posicao
+        global tamanho_corrida
+        posicao += vel_carro
+
+        if posicao == tamanho_corrida:
+            posicao == 0
--- pontos.py
+++ pontos.py
@@ -1,4 +1,5 @@
 import random
+
 def adicao_pontos():
     lista_media = [20, 16, 10]
     nick_cassady = []
@@ -6,13 +7,14 @@ def adicao_pontos():
     oliver_rowland = []
     lista = [nick_cassady, pascal_wehrlein, oliver_rowland]
     for i in range(len(lista)):
-        lista_random = []
-        for j in range(4):                        
-            lista_random.append(random.randint(8, 11))            
-            lista[i].append(random.randint(lista_random[i], lista_media[i]))
+        lista_maxima = [26, 26, 15]
+        for j in range(4):                                             
+            lista[i].append(random.randint(lista_media[i], lista_maxima[i]))
+            
 def media_pontos():
     adicao_pontos()
 
+    
     
     
     
